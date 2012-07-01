19b3f3808510007af65432595c2410d4e1a1c4da
Adam Nowotny <adam.nowotny@gmail.com>
2012-07-01 23:52:31 +1000

    generating toxicity chart based on supplied test data

--- mainModule.js
+++ mainModule.js
@@ -16,7 +16,8 @@ define([
 				selection.treemap = null;
 				selection.toxicity = null;
 			}
-		};
+		},
+		metrics = ['complexity', 'lines'];
 
 	function mainModule () {
 		navigation.fileLoaded(parseMetricFile).chartChanged(updateChart)();
@@ -32,8 +33,9 @@ define([
 	function updateChart(name, /* optional */ metricName) {
 		if (!chartData) return;
 		$('.hero-unit').slideUp('slow', function () {
-			$('.chart:not(.' + name + ')').hide('slow');
-			$('.' + name).show('slow');
+			var selector = '.' + name;
+			$('.chart:not('+ selector + ')').hide('slow');
+			$(selector).show('slow');
 			if (name === 'treemap') {
 				showTreemap(metricName);
 			}
@@ -57,12 +59,42 @@ define([
 	function showToxicity () {
 		$('.toxicity').html('');
 		if (!selection.toxicity) {
-				selection.toxicity = d3.select('.toxicity').data([chartData]);
+			var fxcopData = [
+				{
+					name: 'class1',
+					complexity: 10,
+					lines: 20
+				},
+				{
+					name: 'class2',
+					complexity: 60,
+					lines: 40
+				}
+			];
+			var toxicityData = groupByLayer(fxcopData);
+			selection.toxicity = d3.select('.toxicity').data([toxicityData]);
 		}
 		toxicity.width($('.chart-container').width())
 			.height($('.chart-container').height());
 		toxicity(selection.toxicity);
 	}
 
+	function groupByLayer (data) {
+		var extractMetric = function (i, metricName) {
+			var stackEntry = function (i, d) {
+				return {
+					x: i,
+					y: d[metricName],
+					name: d.name
+				};
+			};
+			return {
+				metric: metricName,
+				values: $(data).map(stackEntry).toArray()
+			};
+		};
+
+		return $(metrics).map(extractMetric).toArray();
+	}
 	return mainModule;
 });
--- toxicityChart.js
+++ toxicityChart.js
@@ -7,50 +7,63 @@ define(['d3'], function (d3) {
 		chart = null;
 
 	function toxicityChart(selection) {
-		selection.each(function(d,i) {
+		selection.each(function(data, i) {
+			var layersCount = data.length,
+				barsCount = data[0].values.length,
+					layersData = d3.layout.stack()
+					.values(function(d) { return d.values; })
+					(data),
+				maxY = maxBarValue(layersData),
+				x = function(d) { return d * width / barsCount;	},
+				y0 = function(d) { return height - d.y0 * height / maxY; },
+				y1 = function(d) { return height - (d.y + d.y0) * height / maxY; };
+
 			chart = d3.select(this)
 				.append("svg")
 				.attr("width", width)
 				.attr("height", height + labelsHeight);
 
 			var layers = chart.selectAll("g.layer")
-				.data(data)
+				.data(layersData)
 				.enter().append("g")
-				.style("fill", function(d, i) { return color(i / (n - 1)); })
+				.style("fill", function(d, i) { return color(i / (layersCount - 1)); })
 				.attr("class", "layer");
 
 			var bars = layers.selectAll("g.bar")
-				.data(function(d) { return d; })
+				.data(function(d) { return d.values; })
 				.enter().append("g")
 				.attr("class", "bar")
-				.attr("transform", function(d) { return "translate(" + x(d) + ",0)"; });
+				.attr("transform", function(d) { return "translate(" + x(d.x) + ",0)"; });
 
 			bars.append("rect")
-				.attr("width", x({x: 0.9}))
+				.attr("width", x(0.9))
 				.attr("x", 0)
 				.attr("y", height)
 				.attr("height", 0)
 				.transition()
+				.duration(1000)
 				.delay(function(d, i) { return i * 10; })
 				.attr("y", y1)
 				.attr("height", function(d) { return y0(d) - y1(d); });
 
 			var labels = chart.selectAll("text.label")
-				.data(data[0])
+				.data(layersData[0].values)
 				.enter().append("text")
 				.attr("class", "label")
-				.attr("x", x)
+				.attr("x", function (d, i) { return x(i); })
 				.attr("y", height + 6)
-				.attr("dx", x({x: 0.45}))
+				.attr("dx", x(0.45))
 				.attr("dy", ".71em")
 				.attr("text-anchor", "middle")
-				.text(function(d, i) { return i; });
+				.text(function(d, i) { return d.name; });
+		});
+	}
 
-			chart.append("line")
-				.attr("x1", 0)
-				.attr("x2", width - x({x: 0.1}))
-				.attr("y1", height)
-				.attr("y2", height);
+	function maxBarValue (data) {
+		return d3.max(data, function(d) {
+			return d3.max(d.values, function(d) {
+				return d.y0 + d.y;
+			});
 		});
 	}
 
@@ -62,64 +75,10 @@ define(['d3'], function (d3) {
 
 	toxicityChart.height = function (value) {
 		if (!arguments.length) return height;
-		height = value;
+		height = value - labelsHeight;
 		return toxicityChart;
 	};
 
-	var n = 4, // number of layers
-	m = 64, // number of samples per layer
-	data = d3.layout.stack()(stream_layers(n, m, 0.1));
-
-	var mx = m,
-		my = d3.max(data, function(d) {
-			return d3.max(d, function(d) {
-				return d.y0 + d.y;
-			});
-		}),
-		mz = d3.max(data, function(d) {
-			return d3.max(d, function(d) {
-				return d.y;
-			});
-		}),
-		x = function(d) { return d.x * width / mx; },
-		y0 = function(d) { return height - d.y0 * height / my; },
-		y1 = function(d) { return height - (d.y + d.y0) * height / my; },
-		y2 = function(d) { return d.y * height / mz; }; // or `my` to not rescale
-
-	/* Inspired by Lee Byron's test data generator. */
-	function stream_layers(n, m, o) {
-		if (arguments.length < 3) o = 0;
-		function bump(a) {
-			var x = 1 / (0.1 + Math.random()),
-			y = 2 * Math.random() - 0.5,
-			z = 10 / (0.1 + Math.random());
-			for (var i = 0; i < m; i++) {
-				var w = (i / m - y) * z;
-				a[i] += x * Math.exp(-w * w);
-			}
-		}
-		return d3.range(n).map(function() {
-			var a = [], i;
-			for (i = 0; i < m; i++) a[i] = o + o * Math.random();
-				for (i = 0; i < 5; i++) bump(a);
-					return a.map(stream_index);
-			});
-	}
-
-	/* Another layer generator using gamma distributions. */
-	function stream_waves(n, m) {
-		return d3.range(n).map(function(i) {
-			return d3.range(m).map(function(j) {
-				var x = 20 * j / m - i / 3;
-				return 2 * x * Math.exp(-0.5 * x);
-			}).map(stream_index);
-		});
-	}
-
-	function stream_index(d, i) {
-		return {x: i, y: Math.max(0, d)};
-	}
-
 	return toxicityChart;
 
-	});
+});
